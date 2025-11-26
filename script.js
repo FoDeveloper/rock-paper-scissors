f6fe0efd2d70fc9d5376e69811f86e77b73dbfaa
FoDeveloper <168691483+FoDeveloper@users.noreply.github.com>
2025-11-25 23:15:40 -0600

    Added getComputerChoice function, made it arrow function and ternary conditions

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+// Testing JS is linked to the HTML file //
+
+// console.log("Hello World");
+
+// Create a function getComputerChoice that randomly returns one of the following string variables: rock, paper, scissors. //
+// A random output dictates which of the string variables is displayed. //
+// The random output is a number variable, so assigning each of the three variable to a third of the output range could achieve our goal.//
+
+//function getComputerChoice() {
+//  return Math.floor(Math.random() * 3);
+// }
+
+let getComputerChoice = () => {
+    return Math.floor(Math.random() * 3) == 0 ?  "Rock" : Math.floor(Math.random() * 3) == 1 ?  "Paper" : "Scissors";}
+
+console.log(getComputerChoice());
