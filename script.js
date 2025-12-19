74f64262c50fd2f9e8c2694b37900d9de0d176fc
FoDeveloper <168691483+FoDeveloper@users.noreply.github.com>
2025-12-19 00:26:00 -0600

    updated UI

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
 let getComputerChoice = () => {
     return Math.floor(Math.random() * 3) === 0 ?  "rock" : Math.floor(Math.random() * 3) === 1 ?  "paper" : "scissors";}
 
-let getHumanChoice = () => prompt("Rock, Paper, or Scissors?");
-
-let playGame = () => {
-let humanScore = 0;
-let computerScore = 0;
-
 let playRound = (humanChoice, computerChoice) => {
     let humanNormalized = humanChoice.toLowerCase();
-    let result;
-    let message;
     if ((humanNormalized === "rock" && computerChoice === "paper") || (humanNormalized === "scissors" && computerChoice === "rock") || (humanNormalized === "paper" && computerChoice === "scissors")) {
         result = "Loser!";
         message = `${result} ${computerChoice.toUpperCase()} beats ${humanNormalized.toUpperCase()}`;}
@@ -28,27 +20,12 @@ let playRound = (humanChoice, computerChoice) => {
         result,
         message};}
 
-for (let i = 0; i <5; i++) {
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-const round = playRound(humanSelection,computerSelection);
-if (round.result === "Loser!"){
-    ++computerScore
-}
-else if (round.result === "Winner!") {
-    ++humanScore}
-alert(`CPU Choice: ${computerSelection}, Human Choice: ${humanSelection}. 
-${round.message}. 
-SCORE: Human:${humanScore} CPU:${computerScore}`);
-}
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
 
-if(humanScore > computerScore) {
-    alert (`Human wins ${humanScore} - ${computerScore}`)
-}
-else if (humanScore === computerScore) {
-    alert ('Draw.')
-}
-else {alert (`CPU wins ${computerScore} - ${humanScore}`)}
-}
+rock.addEventListener("click", playRound);
 
-(playGame());
+paper.addEventListener("click", playRound);
+
+scissors.addEventListener("click", playRound);
