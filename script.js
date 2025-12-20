9b5d4e30bf270b1f7fd01737922b26a413989b3c
FoDeveloper <168691483+FoDeveloper@users.noreply.github.com>
2025-12-20 01:56:25 -0600

    Updated rps UI

--- script.js
+++ script.js
@@ -1,31 +1,30 @@
 let getComputerChoice = () => {
-    return Math.floor(Math.random() * 3) === 0 ?  "rock" : Math.floor(Math.random() * 3) === 1 ?  "paper" : "scissors";}
+    let random = (Math.random() * 3);
+    return Math.floor(random) === 0 ?  "rock" : Math.floor(random) === 1 ?  "paper" : "scissors";}
+let humanScore = 0;
+let computerScore = 0;
 
 let playRound = (humanChoice, computerChoice) => {
+    let result;
     let humanNormalized = humanChoice.toLowerCase();
     if ((humanNormalized === "rock" && computerChoice === "paper") || (humanNormalized === "scissors" && computerChoice === "rock") || (humanNormalized === "paper" && computerChoice === "scissors")) {
-        result = "Loser!";
-        message = `${result} ${computerChoice.toUpperCase()} beats ${humanNormalized.toUpperCase()}`;}
-    
+        result = "Loser!";}
     else if (humanNormalized === computerChoice) {
-        result = `Tie!`
-        message = `${result} ${humanNormalized.toUpperCase()} is equal to ${computerChoice.toUpperCase()}`;
-    }
-
+        result = `Tie!`;}
     else {
-        result = "Winner!";
-        message = `${result} ${humanNormalized.toUpperCase()} beats ${computerChoice.toUpperCase()}`;
-    }
-    return {
-        result,
-        message};}
+        result = "Winner!";}
+        return(result);}
 
 const rock = document.querySelector("#rock");
 const paper = document.querySelector("#paper");
 const scissors = document.querySelector("#scissors");
 
-rock.addEventListener("click", playRound);
+const buttons = document.querySelectorAll("button");
 
-paper.addEventListener("click", playRound);
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+       const humanChoice = (button.id);
+       playRound(humanChoice, getComputerChoice());
+    }); 
+});
 
-scissors.addEventListener("click", playRound);
